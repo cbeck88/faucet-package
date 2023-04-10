d68bfae294734588961816c18ad82af2a1ad1140
Chris Beck <beck.ct@gmail.com>
2023-04-09 20:57:39 -0600

    add a feature to mobilecoind build

--- build_rust.sh
+++ build_rust.sh
@@ -10,6 +10,6 @@ pushd mobilecoin
 
 export SGX_MODE=HW IAS_MODE=PROD CONSENSUS_ENCLAVE_CSS=$(pwd)/consensus-enclave.css
 
-cargo build --release -p mc-mobilecoind -p mc-mobilecoind-dev-faucet
+cargo build --release -p mc-mobilecoind -p mc-mobilecoind-dev-faucet --feature mc-mobilecoind/bypass-ip-check
 
 popd
